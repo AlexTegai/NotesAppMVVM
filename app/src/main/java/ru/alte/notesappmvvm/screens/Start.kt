dbb4a332b99b852297d53ed262cb1c48f7b9acf5
Aleksandr Tegai <shura.tegay@mail.ru>
2022-05-22 16:14:33 +0300

    Lesson-1

---
+++ app/src/main/java/ru/alte/notesappmvvm/screens/Start.kt
@@ -0,0 +1,12 @@
+package ru.alte.notesappmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController) {
+    
+    Text(text = "Notes App Screen")
+    
+}
